733ec3b43990105098056c943c08b662dcb0eef1
Miikka Silfverberg <miikka.silfverberg@helsinki.fi>
2014-10-03 01:33:33 +0300

    Doxygen compatible documentation for py-files.

--- finnpos/create_splits.py
+++ finnpos/create_splits.py
@@ -1,28 +1,26 @@
-###############################################################################
-#                                                                             #
-# @file    create_splits.py                                                   #
-# @Author  Miikka Silfverberg                                                 #
-# @brief   Divide input file into 80% training data, and 10 % development and #
-#          test data respectively. Assign the first eight of ten consecutive  #
-#          sentences to the training set, the ninth to the development set    #
-#          and the tenth to the training set.                                 #
-#                                                                             #
-###############################################################################
+"""@package create_splits
 
-###############################################################################
-#                                                                             #
-#  (C) Copyright 2014, University of Helsinki                                 #
-#  Licensed under the Apache License, Version 2.0 (the "License");            #
-#  you may not use this file except in compliance with the License.           #
-#  You may obtain a copy of the License at                                    #
-#  http://www.apache.org/licenses/LICENSE-2.0                                 #
-#  Unless required by applicable law or agreed to in writing, software        #
-#  distributed under the License is distributed on an "AS IS" BASIS,          #
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
-#  See the License for the specific language governing permissions and        #
-#  limitations under the License.                                             #
-#                                                                             #
-###############################################################################
+ file    create_splits.py                                                   
+ Author  Miikka Silfverberg                                                 
+ brief   Divide input file into 80% training data, and 10 % development and 
+         test data respectively. Assign the first eight of ten consecutive  
+         sentences to the training set, the ninth to the development set    
+         and the tenth to the training set.                                 
+"""
+
+
+"""
+  (C) Copyright 2014, University of Helsinki                                 
+  Licensed under the Apache License, Version 2.0 (the "License");            
+  you may not use this file except in compliance with the License.           
+  You may obtain a copy of the License at                                    
+  http://www.apache.org/licenses/LICENSE-2.0                                 
+  Unless required by applicable law or agreed to in writing, software        
+  distributed under the License is distributed on an "AS IS" BASIS,          
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   
+  See the License for the specific language governing permissions and        
+  limitations under the License.                                             
+"""
 
 from sys import argv, stderr
 from os  import linesep
--- finnpos/ratna_feats.py
+++ finnpos/ratna_feats.py
@@ -1,26 +1,25 @@
-###############################################################################
-# @file    ratna_feats.py                                                     #
-# @Author  Miikka Silfverberg                                                 #
-# @brief   Extract Ratnaparkhi 1996 features from an input-file.              #
-#                                                                             #
-#          (Adwait Ratnaparkhi: A Maximum Entropy Model for Part-of-Speech    #
-#           Tagging, EMNLP 1996)                                              #
-###############################################################################
+"""@package ratna_feats
 
-###############################################################################
-#                                                                             #
-#  (C) Copyright 2014, University of Helsinki                                 #
-#  Licensed under the Apache License, Version 2.0 (the "License");            #
-#  you may not use this file except in compliance with the License.           #
-#  You may obtain a copy of the License at                                    #
-#  http://www.apache.org/licenses/LICENSE-2.0                                 #
-#  Unless required by applicable law or agreed to in writing, software        #
-#  distributed under the License is distributed on an "AS IS" BASIS,          #
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
-#  See the License for the specific language governing permissions and        #
-#  limitations under the License.                                             #
-#                                                                             #
-###############################################################################
+ file    ratna_feats.py                                                     
+ Author  Miikka Silfverberg                                                 
+ brief   Extract Ratnaparkhi 1996 features from an input-file.              
+                                                                             
+         (Adwait Ratnaparkhi: A Maximum Entropy Model for Part-of-Speech    
+          Tagging, EMNLP 1996)                                              
+"""
+
+"""                                                                            
+  (C) Copyright 2014, University of Helsinki                                 
+  Licensed under the Apache License, Version 2.0 (the "License");            
+  you may not use this file except in compliance with the License.           
+  You may obtain a copy of the License at                                    
+  http://www.apache.org/licenses/LICENSE-2.0                                 
+  Unless required by applicable law or agreed to in writing, software        
+  distributed under the License is distributed on an "AS IS" BASIS,          
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   
+  See the License for the specific language governing permissions and        
+  limitations under the License.                                                                                                                       
+"""
 
 from sys import stdin, stdout, stderr
 from re  import match
