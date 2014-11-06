5edc3945413c856c46f70271fd8f93d66e0df936
Miikka Silfverberg <miikka.silfverberg@helsinki.fi>
2014-11-06 02:31:32 +0200

    Print pic.

---
+++ random/print_pic.c
@@ -0,0 +1,9 @@
+#include    <stdio.h>
+#define x v[1][u - 1]
+#define y v[1][u - 2]
+int main(int u,char*v
+[]){if(!((u-2)%4))pr\
+intf("\n"); for(int z 
+= 1;z<8; ++z) {printf
+("%u",y & 1); y>>=1;}
+if(x)main (++u, v); }
