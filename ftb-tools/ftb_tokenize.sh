424338182b42024218e10d6a944cb1d39e271a3e
Miikka Silfverberg <miikka.silfverberg@helsinki.fi>
2016-01-13 12:10:47 +0200

    Removed duplicate of ftb-tools

--- ftb-tools/ftb_tokenize.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-
-sed 's/$/ sentenceends/'        | 
-hfst-proc2 tokenizer.pmatch.fst |
-grep -v "^$"                    |
-grep -v "^[|]$"                 |
-sed 's/sentenceends//' 
